c92acccae0ec15e10596aeb958c113b5bee5a417
rishiA <rishiapril2000@gmail.com>
2021-04-17 02:45:30 +0530

    cleaned user class

--- server/models/user.py
+++ server/models/user.py
@@ -1,21 +1,6 @@
-from flask_login import UserMixin
-
-class User(UserMixin):
+class User():
     def __init__(self, id, email, password):
         self.id = id
         self.email = email
         self.password = password
         self.authenticated = False
-    
-    def get(self, id, conn):
-        cursor = conn.cursor()
-        sql = "select * from login where id = {}".format(id)
-        cursor.execute(sql)
-        lu = cursor.fetchone()
-        if lu is not None:
-            self.id = lu[0]
-            self.email = lu[1]
-            self.password = lu[2]
-            return self
-        else:
-            return None
